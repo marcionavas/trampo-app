f9206f196b6ae81f98b8e2f18b77b540c1ce0b7a
Marcio Navas <marcioskemp@gmail.com>
2025-10-11 10:29:56 -0300

    Chore: ajuste de imports melhores práticas de importação de modules.

--- apps/users/src/users.service.ts
+++ apps/users/src/users.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@nestjs/common';
-//import { helloCommon } from '../../../libs/common/src';
+import { helloCommon } from '@common';
 
 @Injectable()
 export class UsersService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello World!' + ' ' + helloCommon();
   }
 }
