1230eae094745f5a3d2c2b223e47c3c65d049287
Marcio Navas <marcioskemp@gmail.com>
2025-10-15 19:43:14 -0300

    Feat: ajuste nos arquivos de test e tsconfig*.json

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  moduleFileExtensions: ["js", "json", "ts"],
+  rootDir: ".",
+  testRegex: ".*\\.spec\\.ts$",
+  transform: {
+    "^.+\\.(t|j)s$": "ts-jest"
+  },
+  coverageDirectory: "./coverage",
+  testEnvironment: "node",
+  roots: ["<rootDir>/apps/"],
+  moduleNameMapper: {
+    '^@common$': '<rootDir>/libs/common/src/index.ts',
+    '^@common/(.*)$': '<rootDir>/libs/common/src/$1'
+  }
+};
--- libs/common/src/utils.ts
+++ libs/common/src/utils.ts
@@ -1,3 +1,3 @@
 export function helloCommon(): string {
-  return 'Hello from common lib! JASON e NINA do PAPAIIII!!!';
+  return 'LIBs do aMOR';
 }
